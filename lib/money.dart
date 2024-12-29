1453df1993175ddeddb211eb5187cd2cf7b6e9f9
dicenull <dicenull.dev@gmail.com>
2024-12-29 11:46:55 +0900

    👍 private amount

--- lib/money.dart
+++ lib/money.dart
@@ -1,15 +1,15 @@
 class Dollar {
-  int amount;
+  final int _amount;
 
-  Dollar(this.amount);
+  Dollar(this._amount);
 
   Dollar times(int multiplier) {
-    return Dollar(amount * multiplier);
+    return Dollar(_amount * multiplier);
   }
 
   equals(Object object) {
     Dollar dollar = object as Dollar;
-    return amount == dollar.amount;
+    return _amount == dollar._amount;
   }
 
   @override
