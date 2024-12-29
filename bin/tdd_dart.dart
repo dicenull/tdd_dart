560d4c2d375a32cac12a0437adf42d38cbd7511d
dicenull <dicenull.dev@gmail.com>
2024-12-29 11:10:12 +0900

    ✨ init commit

---
+++ bin/tdd_dart.dart
@@ -0,0 +1,5 @@
+import 'package:tdd_dart/tdd_dart.dart' as tdd_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tdd_dart.calculate()}!');
+}
---
+++ test/tdd_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:tdd_dart/tdd_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
