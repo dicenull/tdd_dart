2a08ac9924f78abefa72f72d317acd47987aa1ce
dicenull <dicenull.dev@gmail.com>
2024-12-30 15:57:07 +0900

    🎨 times実装を引き上げた

--- lib/dollar.dart
+++ lib/dollar.dart
@@ -2,9 +2,4 @@ import 'package:tdd_dart/money.dart';
 
 class Dollar extends Money {
   Dollar(int amount, String name) : super(amount, name);
-
-  @override
-  Money times(int multiplier) {
-    return Money.dollar(amount * multiplier);
-  }
 }
--- lib/franc.dart
+++ lib/franc.dart
@@ -2,9 +2,4 @@ import 'package:tdd_dart/money.dart';
 
 class Franc extends Money {
   Franc(int amount, String name) : super(amount, name);
-
-  @override
-  Money times(int multiplier) {
-    return Franc(amount * multiplier, currencyName);
-  }
 }
