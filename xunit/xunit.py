30131bfbb2a3c6665cf4904195968bce9695ac7c
dicenull <dicenull.dev@gmail.com>
2025-01-01 19:51:59 +0900

    💚 TestResultで失敗が集計される

--- xunit/xunit.py
+++ xunit/xunit.py
@@ -33,6 +33,9 @@ class WasRun(TestCase):
     def tearDown(self):
         self.log = self.log + "tearDown "
 
+    def testBrokenMethod(self):
+        raise Exception
+
 
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):
@@ -44,6 +47,17 @@ class TestCaseTest(TestCase):
         test = WasRun("testMethod")
         result = test.run()
         assert('1 run, 0 failed' == result.summary())
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert('1 run, 1 failed' == result.summary())
+    def testFailedResultFormatting(self):
+        result = TestResult()
+        result.testStarted()
+        result.testFailed()
+        assert('1 run, 1 failed' == result.summary())
 
 TestCaseTest("testTemplateMethod").run()
 TestCaseTest("testResult").run()
+# TestCaseTest("testFailedResult").run()
+TestCaseTest("testFailedResultFormatting").run()
