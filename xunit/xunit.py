772a552dd28fd72e0065fe1d3067648abc6ce377
dicenull <dicenull.dev@gmail.com>
2025-01-01 16:53:47 +0900

    💚👍 テストを実行するとsetUpが呼ばれること

--- xunit/xunit.py
+++ xunit/xunit.py
@@ -1,14 +1,20 @@
 class TestCase:
     def __init__(self, name):
         self.name = name
+    def setUp(self):
+        pass
     def run(self):
         method = getattr(self, self.name)
         method()
+        self.setUp()
 
 class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
         super().__init__(name)
+    
+    def setUp(self):
+        self.wasSetUp = 1
     
     def testMethod(self):
         self.wasRun = 1
@@ -19,5 +25,10 @@ class TestCaseTest(TestCase):
         assert(not test.wasRun)
         test.run()
         assert(test.wasRun)
+    def testSetUp(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert(test.wasSetUp)
 
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
